d3375370e1ad88d330767a7357a547d251469fa8
ardiankgfvy <52664963+ardiankgfvy@users.noreply.github.com>
2020-09-10 16:45:54 +0700

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -80,25 +80,21 @@
           </div>
           <div class="col-md">
              <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Experience</h2>
+              <h2 class="ftco-heading-2">Navigasi</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Adventure</a></li>
-                <li><a href="#" class="py-2 d-block">Hotel and Restaurant</a></li>
-                <li><a href="#" class="py-2 d-block">Beach</a></li>
-                <li><a href="#" class="py-2 d-block">Nature</a></li>
-                <li><a href="#" class="py-2 d-block">Camping</a></li>
-                <li><a href="#" class="py-2 d-block">Party</a></li>
+                <li><a href="{{route('beranda')}}" class="py-2 d-block">Beranda</a></li>
+                <li><a href="{{route('kontak')}}" class="py-2 d-block">Kontak</a></li>
               </ul>
             </div>
           </div>
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<h2 class="ftco-heading-2">Punya Pertanyaan ?</h2>
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Kampung Folley, Distrik Misool Timur, Kabupaten Raja Ampat, Papua Barat</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">0812 4002 0550</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">wpontantun@gmail.com</span></a></li>
 	              </ul>
 	            </div>
             </div>
@@ -108,7 +104,7 @@
           <div class="col-md-12 text-center">
 
             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+              Copyright &copy; Wpontan Ntun Homestay 2020
               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
           </div>
--- resources/views/pages/kontak.blade.php
+++ resources/views/pages/kontak.blade.php
@@ -14,8 +14,8 @@
           		<div class="icon d-flex align-items-center justify-content-center">
           			<span class="icon-map-signs"></span>
           		</div>
-          		<h3 class="mb-2">Address</h3>
-	            <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+          		<h3 class="mb-2">Alamat</h3>
+	            <p>Kampung Folley, Distrik Misool Timur, Kabupaten Raja Ampat, Papua Barat</p>
 	          </div>
           </div>
           <div class="col-md-3 d-flex">
@@ -23,8 +23,17 @@
           		<div class="icon d-flex align-items-center justify-content-center">
           			<span class="icon-phone2"></span>
           		</div>
-          		<h3 class="mb-2">Contact Number</h3>
-	            <p><a href="tel://1234567920">+ 1235 2355 98</a></p>
+          		<h3 class="mb-2">Kontak</h3>
+	            <p><a href="tel://081240020550">0812 4002 0550</a></p>
+	          </div>
+          </div>          
+          <div class="col-md-3 d-flex">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-whatsapp"></span>
+          		</div>
+          		<h3 class="mb-2">Whatsapp</h3>
+	            <p><a href="#">0812 4002 0550</a></p>
 	          </div>
           </div>
           <div class="col-md-3 d-flex">
@@ -32,17 +41,8 @@
           		<div class="icon d-flex align-items-center justify-content-center">
           			<span class="icon-paper-plane"></span>
           		</div>
-          		<h3 class="mb-2">Email Address</h3>
-	            <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-	          </div>
-          </div>
-          <div class="col-md-3 d-flex">
-          	<div class="align-self-stretch box p-4 text-center">
-          		<div class="icon d-flex align-items-center justify-content-center">
-          			<span class="icon-globe"></span>
-          		</div>
-          		<h3 class="mb-2">Website</h3>
-	            <p><a href="#">yoursite.com</a></p>
+          		<h3 class="mb-2">Alamat Email</h3>
+	            <p><a href="mailto:wpontantun@gmail.com">wpontantun@gmail.com</a></p>
 	          </div>
           </div>
         </div>
@@ -52,29 +52,12 @@
 		<section class="ftco-section contact-section">
       <div class="container">
         <div class="row block-9">
-          <div class="col-md-6 order-md-last d-flex">
-            <form action="#" class="bg-light p-5 contact-form">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Name">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Email">
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Subject">
-              </div>
-              <div class="form-group">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-              </div>
-              <div class="form-group">
-                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
-              </div>
-            </form>
-          
+          <div class="col-md-6 d-flex">
+            <img class="" src="{{ asset('welcome.jpg') }}" alt="Trulli" width="545" height="400">          
           </div>
 
           <div class="col-md-6 d-flex">
-          	<div id="map" class="bg-white"></div>
+          	<img class="" src="{{ asset('viewmalam.jpg') }}" alt="Trulli" width="545" height="400">
           </div>
         </div>
       </div>
