7241bf955544ac5d4ceb67702d60123d4f7b3989
ardiankgfvy <52664963+ardiankgfvy@users.noreply.github.com>
2020-09-10 15:24:02 +0700

    login reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
-
+<title>Atur Ulang Kata Sandi</title>
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+            <div class="card search-wrap-1 ftco-animate">
+                <div class="card-header kuning">
+                    <h3 align="center">
+                     Atur Ulang Sandi
+                     </h3> 
+                 </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -14,7 +19,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right putih">Email</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
@@ -28,7 +33,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right putih">Kata Sandi</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -42,7 +47,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right putih">Konfirmasi Kata Sandi</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,79 +2,65 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/animate.css') }}">
+    
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/owl.theme.default.min.css"') }}">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/magnific-popup.css') }}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/aos.css') }}">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/ionicons.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/jquery.timepicker.css') }}">
+
+    
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ asset('vhomestay/css/style.css') }}">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  <style>
+    .search-wrap-1 {
+        margin-top: 20%;
+    }
+    .kuning{
+        background-color: #f9ab30 ;
+    }
+    .putih{
+        color: white;
+    }
+  </style>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        @yield('content')
 
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <script src="{{ asset('vhomestay/js/jquery.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/jquery-migrate-3.0.1.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/popper.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/jquery.easing.1.3.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/jquery.stellar.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/jquery.magnific-popup.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/aos.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/jquery.animateNumber.min.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/bootstrap-datepicker.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/scrollax.min.js') }}"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="{{ asset('vhomestay/js/google-map.js') }}"></script>
+  <script src="{{ asset('vhomestay/js/main.js') }}"></script>
 </body>
 </html>
