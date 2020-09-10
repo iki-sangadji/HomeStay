964ad633abb806eb472b8bfffc366cfc445cd3bf
ardiankgfvy <52664963+ardiankgfvy@users.noreply.github.com>
2020-09-10 18:06:21 +0700

    edit sdkit

--- resources/views/pages/galeri.blade.php
+++ resources/views/pages/galeri.blade.php
@@ -3,7 +3,13 @@
 @section('content')
     
     <section class="hero-wrap hero-wrap-2" style="background-image: url('{{ asset('clown-fish.jpg') }}');" data-stellar-background-ratio="0.5">
-        
+        <div class="container">
+            <div class="row justify-content-center pb-4">
+                <div class="col-md-12 heading-section text-center ftco-animate">
+                    <h2 class="mb-4 tekstengahbawah">Galeri</h2>
+                </div>
+            </div>
+        </div>
     </section>
     <section class="ftco-section">
         <div class="container">
