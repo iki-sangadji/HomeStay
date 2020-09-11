0e0523a090c5f3880d64f70e018926d2202b7ec8
ardiankgfvy <52664963+ardiankgfvy@users.noreply.github.com>
2020-09-11 14:41:27 +0700

    crud wisata

--- resources/views/pages/paketwisata.blade.php
+++ resources/views/pages/paketwisata.blade.php
@@ -15,66 +15,74 @@
     <section class="ftco-section">
         <div class="container">
             <div class="row justify-content-center pb-4">
-          <div class="col-md-12 heading-section text-center ftco-animate">
-            <h2 class="mb-4">Paket Trip Wisata</h2>
-            <p>Trip pada homestay waponta ntun hanya sekali jalan untuk beberapa tempat wisata.
-                <br> Tarif trip sekali jalan : <b> 4.000.000</b>
-            </p>
-          </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 ftco-animate mb-4">
-                <div class="project-destination">
-                    <a href="#" class="img" style="background-image: url('{{ asset('Pulau Banoz.jpg') }}');">
-                        <div class="text">
-                            <h3>Pulau Banoz</h3>
-                            <span>1</span>
-                        </div>
-                    </a>
-                </div>
+            <div class="col-md-12 heading-section text-center ftco-animate">
+                <a href="{{route('tambah_paket_wisata')}}" class="btn btn-success">Tambah Paket Trip Wisata</a>
+                <h2 class="mb-4">Paket Trip Wisata</h2>
+                <hr>
             </div>
-            <div class="col-md-3 ftco-animate mb-4">
-                <div class="project-destination">
-                    <a href="#" class="img" style="background-image: url('{{ asset('Tangga Seribu.jpg') }}');">
-                        <div class="text">
-                            <h3>Tangga Seribu</h3>
-                            <span>2</span>
-                        </div>
-                    </a>
-                </div>
+            <div class="col-md-12 heading-section text-center ftco-animate">
+                <h4>Trip 1</h4>
+                <p>Trip ke <b>1</b>  hanya sekali jalan untuk beberapa tempat wisata.
+                    <br> Tarif trip sekali jalan : <b> 4.000.000</b>
+                </p> 
+                    <a href="#" class="btn btn-danger">Hapus Paket</a>
             </div>
-            <div class="col-md-3 ftco-animate mb-4">
-                <div class="project-destination">
-                    <a href="#" class="img" style="background-image: url('{{ asset('Goa Puteri Termenung.jpg') }}');">
-                        <div class="text">
-                            <h3>Goa Puteri Termenung</h3>
-                            <span>3</span>
-                        </div>
-                    </a>
-                </div>
             </div>
-            <div class="col-md-3 ftco-animate mb-4">
-                <div class="project-destination">
-                    <a href="#" class="img" style="background-image: url('{{ asset('Danau Love.jpg') }}');">
-                        <div class="text">
-                            <h3>Danau Love</h3>
-                            <span>4</span>
-                        </div>
-                    </a>
+            <div class="row">
+                <div class="col-md-3 ftco-animate mb-4">
+                    <div class="project-destination">
+                        <a href="#" class="img" style="background-image: url('{{ asset('Pulau Banoz.jpg') }}');">
+                            <div class="text">
+                                <h3>Pulau Banoz</h3>
+                                <span>1</span>
+                            </div>
+                        </a>
+                    </div>
                 </div>
+                <div class="col-md-3 ftco-animate mb-4">
+                    <div class="project-destination">
+                        <a href="#" class="img" style="background-image: url('{{ asset('Tangga Seribu.jpg') }}');">
+                            <div class="text">
+                                <h3>Tangga Seribu</h3>
+                                <span>2</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 ftco-animate mb-4">
+                    <div class="project-destination">
+                        <a href="#" class="img" style="background-image: url('{{ asset('Goa Puteri Termenung.jpg') }}');">
+                            <div class="text">
+                                <h3>Goa Puteri Termenung</h3>
+                                <span>3</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 ftco-animate mb-4">
+                    <div class="project-destination">
+                        <a href="#" class="img" style="background-image: url('{{ asset('Danau Love.jpg') }}');">
+                            <div class="text">
+                                <h3>Danau Love</h3>
+                                <span>4</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3 ftco-animate mb-4">
+                    <div class="project-destination">
+                        <a href="#" class="img" style="background-image: url('{{ asset('Batu Pensil.jpg') }}');">
+                            <div class="text">
+                                <h3>Batu Pensil</h3>
+                                <span>5</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>        
+                
+                
             </div>
-            <div class="col-md-3 ftco-animate mb-4">
-                <div class="project-destination">
-                    <a href="#" class="img" style="background-image: url('{{ asset('Batu Pensil.jpg') }}');">
-                        <div class="text">
-                            <h3>Batu Pensil</h3>
-                            <span>5</span>
-                        </div>
-                    </a>
-                </div>
-            </div>           
-            
-        </div>
         </div>
     </section>
+
 @endsection
