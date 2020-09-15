697d614ee1e6f8d49a8978b94744e4017c8a7be8
7osua <j7.manullang@gmail.com>
2020-09-15 16:33:44 +0700

    Hapus button [edit,hapus] galeri di beranda

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -116,34 +116,6 @@
                                 </div>
                             </a>
                         </div>
-                        @auth
-                        <div class="mt-1 bg-white d-flex justify-content-end">
-                            <a href="{{route('edit_galeri',$row->id_galeri)}}">
-                                <button class="btn btn-sm btn-primary mr-1">
-                                    <span class="align-middle">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                        </svg>
-                                        Edit
-                                    </span>
-                                </button>
-                            </a>
-                            <form onsubmit="return confirm('Yakin Ingin Menghapus?');" action="{{route('delete_galeri',$row->id_galeri)}}" method="get" >
-                                {{ csrf_field() }}
-                                <input type="hidden" name="_method" value="DELETE">
-                                
-                                <button class="btn btn-primary btn-sm" type="submit"> 
-                                    <span class="align-middle">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
-                                        </svg>
-                                        Hapus
-                                    </span>
-                                </button>
-                            </form>
-                        </div>
-                        @endauth
                     </div>
                 
             @endforeach
@@ -151,70 +123,4 @@
     </div>
 </section>
 
-	{{-- <section class="ftco-section">
-		<div class="container">
-			<div class="row ftco-animate justify-content-center pb-4">
-				<div class="col-md-12 heading-section text-center ftco-animate">
-					<h2 class="mb-4">Detail Kamar</h2>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-6 ftco-animate">
-					<div class="card mb-3" style="height:50%">
-						<img src="{{asset('images/kamar_woponta_ntu.jpeg')}}" class="img-fluid" alt="Tipe Kamar Woponta Ntu" style="width:100%; height:500px !important;">
-						<div class="card-body">
-							<h5 class="card-title">Double Bed</h5>
-							<p class="card-text">
-								This is a wider card with supporting text below <br> 
-								as a natural lead-in to additional content. <br> 
-								This content is a little bit longer.
-								This is a wider card with supporting text below <br> 
-								as a natural lead-in to additional content. <br> 
-								This content is a little bit longer.
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-6 ftco-animate">
-					<div class="card mb-3" style="height:50%">
-						<img src="{{asset('images/kamar_woponta_ntu.jpeg')}}" class="img-fluid" alt="Tipe Kamar Woponta Ntu" style="width:100%; height:500px !important;">
-						<div class="card-body">
-							<h5 class="card-title">Single Bed</h5>
-							<p class="card-text">
-								This is a wider card with supporting text below <br> 
-								as a natural lead-in to additional content. <br> 
-								This content is a little bit longer.
-								This is a wider card with supporting text below <br> 
-								as a natural lead-in to additional content. <br> 
-								This content is a little bit longer.
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section class="ftco-counter img mb-4" id="section-counter">
-		<div class="container">
-			<div class="row d-flex ftco-animate">
-				<div class="col-md-6 d-flex">
-					<div class="img d-flex align-self-stretch" style="background-image:url({{asset('images/frontside_woponta_ntu.jpeg')}});"></div>
-				</div>
-				<div class="col-md-6 pl-md-5 py-5">
-					<div class="row justify-content-start pb-3">
-						<div class="col-md-12 heading-section ftco-animate">
-							<h2 class="mb-4">Menambah kenyamanan Anda, Tersedia :</h2>
-							<p class="text-justify">
-								Fan	<br>
-								Hammock <br>
-								Electric plugs<br>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			{{-- <hr> 
-		</div>
-	</section> --}}
 @endsection
